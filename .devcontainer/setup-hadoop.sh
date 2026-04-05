be61159c8cb38e9f13648f1110438b45b1570d58
SartMa <sarthaktechindia@gmail.com>
2026-04-05 19:54:30 +0530

    Fix Codespace devcontainer PATH issue breaking VS Code server

--- .devcontainer/setup-hadoop.sh
+++ .devcontainer/setup-hadoop.sh
@@ -4,5 +4,10 @@ if [ ! -d "/opt/hadoop" ]; then
   wget https://archive.apache.org/dist/hadoop/common/hadoop-3.3.6/hadoop-3.3.6.tar.gz -O /tmp/hadoop.tar.gz
   sudo tar -xzf /tmp/hadoop.tar.gz -C /opt
   sudo mv /opt/hadoop-3.3.6 /opt/hadoop
-  rm /tmp/hadoop.tar.gz
+  sudo rm /tmp/hadoop.tar.gz
 fi
+
+# Add environment variables to the interactive shell profile
+echo 'export HADOOP_HOME=/opt/hadoop' >> ~/.bashrc
+echo 'export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin' >> ~/.bashrc
+echo 'export HADOOP_CLASSPATH=$(pwd)/opennlp-tools-1.9.3.jar' >> ~/.bashrc
